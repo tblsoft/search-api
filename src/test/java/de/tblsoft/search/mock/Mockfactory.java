a4d1b9e06b2b5f06be9d5e331fe3c54b477fdb7f
Thomas Bärwinkel <tbl@gmx.de>
2017-12-17 01:06:47 +0100

    improve the http servlet mock

--- src/test/java/de/tblsoft/search/mock/Mockfactory.java
+++ src/test/java/de/tblsoft/search/mock/Mockfactory.java
@@ -6,10 +6,13 @@ import org.mockito.Mockito;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by tbl on 25.11.17.
@@ -27,22 +30,45 @@ public class Mockfactory {
                     .withKeyValueSeparator("=")
                     .split(uri.getQuery());
 
+            final Map<String, String[]> decodedParametersMap = parametersMap.
+                    entrySet().
+                    stream().
+                    collect(Collectors.toMap(
+                            Map.Entry::getKey, entry -> decodeUrlParam(entry.getValue())));
+
+
+            //map(e -> decodeUrlParam(e.getValue()))
+
 
             HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
             Enumeration<String> parameterNames = Collections.enumeration(parametersMap.keySet());
             Mockito.when(httpServletRequest.getParameterNames()).thenReturn(parameterNames);
             for (String key : parametersMap.keySet()) {
-                Mockito.when(httpServletRequest.getParameter(key)).thenReturn(parametersMap.get(key));
+                Mockito.when(httpServletRequest.getParameter(key)).thenReturn(decodedParametersMap.get(key)[0]);
             }
 
             Mockito.when(httpServletRequest.getHeaderNames()).thenReturn(parameterNames);
             Cookie[] cookies = {};
             Mockito.when(httpServletRequest.getCookies()).thenReturn(cookies);
 
+            Mockito.when(httpServletRequest.getRequestURI()).thenReturn(uri.getPath());
+
+            Mockito.when(httpServletRequest.getParameterMap()).thenReturn(decodedParametersMap);
+
             return httpServletRequest;
 
         } catch (Exception e ) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static String[] decodeUrlParam(String param) {
+        try {
+            String[] paramArray = new String[] {URLDecoder.decode(param, "UTF-8")};
+            return paramArray;
+        } catch (UnsupportedEncodingException e) {
+            String[] paramArray = new String[] {param};
+            return paramArray;
         }
     }
 
