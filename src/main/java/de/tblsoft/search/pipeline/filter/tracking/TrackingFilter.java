f384e2f04b68bdd38506d8101c5fdb8856d74de7
Thomas Bärwinkel <tbl@gmx.de>
2017-12-17 01:05:14 +0100

    implement a tracking filter

---
+++ src/main/java/de/tblsoft/search/pipeline/filter/tracking/TrackingFilter.java
@@ -0,0 +1,15 @@
+package de.tblsoft.search.pipeline.filter.tracking;
+
+import de.tblsoft.search.pipeline.PipelineContainer;
+import de.tblsoft.search.pipeline.filter.AbstractFilter;
+
+/**
+ * Created by tbl on 02.12.17.
+ */
+public class TrackingFilter extends AbstractFilter {
+
+    @Override
+    public PipelineContainer filter(PipelineContainer pipelineContainer) throws Exception {
+        return pipelineContainer;
+    }
+}
